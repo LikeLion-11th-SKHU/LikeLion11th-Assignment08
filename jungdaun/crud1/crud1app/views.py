11acd973610bcfd114a34b7d2e74e76a9d697501
wjdekdns1028 <wjdekdns0218@naver.com>
2023-05-28 23:22:12 +0900

    commit1

---
+++ jungdaun/crud1/crud1app/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def new(request):
+    return render(request, 'new.html')
+    
+def create(request):
+    blog = Blog()
+    blog.title = request.POST['title']
+    blog.pub_date = timezone.now()
+    blog.body = request.POST['body']
+    blog.body = request.POST['username']
+    blog.body = request.POST['useremail']
+    blog.body = request.POST['userword']
+    blog.save()
+    return redirect('read')
+
+def read(request):
+    blogs = Blog.objects
+    return render(request, 'read.html', {'blogs' : blogs})
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, id = id)
+    return render(request, 'detail.html', {'blog' : blog})
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id = id)
+    return render(request, 'edit.html', {'edit_blog' : edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id = id)
+    update_blog.title = request.POST['title']
+    update_blog.pub_date = timezone.now()
+    update_blog.body = request.POST['body']
+    update_blog.body = request.POST['username']
+    update_blog.body = request.POST['useremail']
+    update_blog.body = request.POST['userword']
+    update_blog.save()
+    return redirect('read') 
+
+def delete(request, id):
+    delete_blog = get_object_or_404(Blog, id = id)
+    delete_blog.delete()
+    return redirect('read') 
